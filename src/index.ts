d90a1300eb15fa70e733e17260a9791c173743cd
Alan Martín López Chávez <alan.m.l.ch.1997@gmail.com>
2022-08-04 10:45:03 -0600

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const puppeteer = require('puppeteer');
 const visitanyPage = async () => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
-    await page.goto("https://phys.org/");
+    await page.goto("https://phys.org/news/2022-08-miniature-lens-atoms.html");
     await page.screenshot({path: "physPage.png"});
 
     await browser.close();
