e2ef0435e88f22651ad114be41b939c8a34f80e3
Alan Martín López Chávez <alan.m.l.ch.1997@gmail.com>
2022-08-07 20:18:57 -0600

    initial commit
    Agregué poder tomar captura a 3 sitios a la vez. Luego lo depuro para que se vea más bonito y hago una función que llame las URL de un array

--- src/index.js
+++ src/index.js
@@ -12,11 +12,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const puppeteer = require("puppeteer");
 //const puppeteer = require('puppeteer');
 let url = "https://phys.org/news/2022-08-miniature-lens-atoms.html";
+let url2 = "https://www.tematika.com/libros?limit=40&p=1";
+let url3 = "https://fauux.neocities.org/yearning.html";
 const visitanyPage = (url) => __awaiter(void 0, void 0, void 0, function* () {
     const browser = yield puppeteer.launch({ headless: false });
     const page = yield browser.newPage();
+    let urlName = url.toString().replace(/(https:\/\/|\.)/g, "").replace(/(?=\/)([\s\S]+)/g, "");
     yield page.goto(url);
-    yield page.screenshot({ path: "physPage.png" });
+    yield page.screenshot({ path: `${urlName}.png` });
     yield browser.close();
 });
 visitanyPage(url);
+visitanyPage(url2);
+visitanyPage(url3);
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,19 @@
 import * as puppeteer from "puppeteer"
 //const puppeteer = require('puppeteer');
 let url = "https://phys.org/news/2022-08-miniature-lens-atoms.html";
+let url2 = "https://www.tematika.com/libros?limit=40&p=1";
+let url3 = "https://fauux.neocities.org/yearning.html";
 
 const visitanyPage = async (url:string):Promise<any> => {
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
+    let urlName = url.toString().replace(/(https:\/\/|\.)/g, "").replace(/(?=\/)([\s\S]+)/g, "");
     await page.goto(url);
-    await page.screenshot({path: "physPage.png"});
+    await page.screenshot({path: `${urlName}.png`});
 
     await browser.close();
 };
 
 visitanyPage(url);
+visitanyPage(url2);
+visitanyPage(url3);
